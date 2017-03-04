36d761839301aa415127878a497e2c579233bb55
Christopher Lo <celo@ucsd.edu>
2017-03-03 18:17:48 -0800

    navbar changes, faq and wholesale done

--- js/clickndig.js
+++ js/clickndig.js
@@ -2,3 +2,22 @@ $('.launch-modal').on('click', function(e){
     e.preventDefault();
     $( '#' + $(this).data('modal-id') ).modal();
 });
+
+
+/* WHOLESALE PAGE */
+$('#reason').on('change', function(e) {
+    e.preventDefault();
+    console.log($(this).val());
+    if($(this).val() === 'Other (Please Describe)') {
+        console.log('yhe');
+        $('#otherdesc').show();
+    }
+});
+$('#othercheck').click(function(){
+            if($(this).is(":checked")){
+                $('#otherdesc1').show();
+            }
+            else if($(this).is(":not(:checked)")){
+                $('#otherdesc1').hide();
+            }
+        });
